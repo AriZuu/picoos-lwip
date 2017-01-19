879027674ca6aa0794ab8497e2ed5ff934e822c8
Ari Suutari <ari@stonepile.fi>
2017-01-19 19:40:49 +0200

    Remove ALIGNED macro. It was not used and conflicted with definitions in WICED 3.7.0 SDK.

--- include/arch/cc.h
+++ include/arch/cc.h
@@ -78,7 +78,6 @@ typedef uintptr_t          mem_ptr_t;
 #define PACK_STRUCT_STRUCT __attribute__ ((__packed__))
 #define PACK_STRUCT_END
 #define PACK_STRUCT_FIELD(fld) fld
-#define ALIGNED(n)  __attribute__((aligned (n)))
 
 int sys_random(void);
 #define LWIP_RAND() sys_random()
